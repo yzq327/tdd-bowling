387e67670df5be46b9ec2017ef143a23c82dd07e
Your Name <you@example.com>
2020-09-14 19:22:49 +0800

    BowlingGame-Refactor

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -4,6 +4,7 @@ public class BowlingGame {
     private static int frame_number = 10;
     private static int throw_number = 0;
     private static int bestScore=10;
+    private static int[]frame_score;
 
 
 
@@ -14,34 +15,56 @@ public class BowlingGame {
         int len = scoreNumbers.length;
         for (int i=0; i<len;i++){
             if(throw_number == 0){ //该轮的第一次投球
-                if(scoreNumbers[i]<bestScore){  //分数小于10
-                    frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i];
-                    throw_number ++;
-                }else {  //分数等于10
-                    if(whichFrame == frame_number-1){
-                        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i];
-                    }else{
-                        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i]+scoreNumbers[i+1]+scoreNumbers[i+2];
-                        whichFrame ++;
-                    }
-                }
+                whichFrame = firstThrow(scoreNumbers, whichFrame, frame_score, i);
             }else if(throw_number == 1){   //该轮的第二次投球
-                if(scoreNumbers[i-1]+scoreNumbers[i] < bestScore){ //分数小于10
-                    frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i];
-                    whichFrame ++;
-                }else{//分数等于10
-                    if(whichFrame == frame_number-1){
-                        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i];
-                    }else{
-                        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i]+scoreNumbers[i+1];
-                        whichFrame ++;
-                    }
-                }
-                throw_number --;
+                whichFrame = secondThrow(scoreNumbers, whichFrame, frame_score, i);
             }
         }
         int totalScore=sumFrameScore(frame_score);
         return totalScore;
+    }
+
+    private int secondThrow(int[] scoreNumbers, int whichFrame, int[] frame_score, int i) {
+        if(scoreNumbers[i -1]+ scoreNumbers[i] < bestScore){ //分数小于10
+            isNormal(scoreNumbers, whichFrame, frame_score, i);
+            whichFrame++;
+        }else{//分数等于10
+            if(whichFrame == frame_number-1){
+                isNormal(scoreNumbers, whichFrame, frame_score, i);
+            }else{
+                isSpare(scoreNumbers, whichFrame, frame_score, i);
+                whichFrame++;
+            }
+        }
+        throw_number --;
+        return whichFrame;
+    }
+
+    private int firstThrow(int[] scoreNumbers, int whichFrame, int[] frame_score, int i) {
+        if(scoreNumbers[i]<bestScore){  //分数小于10
+            isNormal(scoreNumbers, whichFrame, frame_score, i);
+            throw_number ++;
+        }else {  //分数等于10
+            if(whichFrame == frame_number-1){
+                isNormal(scoreNumbers, whichFrame, frame_score, i);
+            }else{
+                isStrike(scoreNumbers, whichFrame, frame_score, i);
+                whichFrame++;
+            }
+        }
+        return whichFrame;
+    }
+
+    private void isSpare(int[] scoreNumbers, int whichFrame, int[] frame_score, int i) {
+        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i]+ scoreNumbers[i +1];
+    }
+
+    private void isStrike(int[] scoreNumbers, int whichFrame, int[] frame_score, int i) {
+        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i]+ scoreNumbers[i +1]+ scoreNumbers[i +2];
+    }
+
+    private void isNormal(int[] scoreNumbers, int whichFrame, int[] frame_score, int i) {
+        frame_score[whichFrame] = frame_score[whichFrame] + scoreNumbers[i];
     }
 
     private static int sumFrameScore(int[] frame_score){
@@ -53,17 +76,9 @@ public class BowlingGame {
         return totalSCore;
     }
 
-    private static void isNormal(){
 
-    }
 
-    private static void isStrike(){
 
-    }
-
-    private static void isSpare(){
-
-    }
 
 
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -14,27 +14,16 @@ public class BowlingGameTest {
     @Test
     void should_return_total_score(){
         int[] scoreNumbers1={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-      //int[] scoreNUmbers2={10,10,10,10,10,10,10,10,10,10,10,10};
+        int[] scoreNUmbers2={10,10,10,10,10,10,10,10,10,10,10,10};
+        int[] scoreNUmbers3={1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,6};
         int totalScore1=bowlinggame.countScore(scoreNumbers1);
+        int totalScore2=bowlinggame.countScore(scoreNUmbers2);
+        int totalScore3=bowlinggame.countScore(scoreNUmbers3);
         int Excepted1=0;
-        //int totalScore2=bowlinggame.countScore(scoreNUmbers2);
-        //int Excepted2=300;
-        assertEquals(Excepted1, totalScore1);
-       // assertEquals(Excepted2, totalScore2);
-    }
-    @Test
-    void should_return_total_score2(){
-        int[] scoreNUmbers2={10,10,10,10,10,10,10,10,10,9,1,10};
-        int totalScore2=bowlinggame.countScore(scoreNUmbers2);
-        int Excepted2=279;
-        assertEquals(Excepted2, totalScore2);
-    }
-    @Test
-    void should_return_total_score3(){
-        int[] scoreNUmbers2={1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,6};
-        int totalScore2=bowlinggame.countScore(scoreNUmbers2);
-        int Excepted2=123;
-        assertEquals(Excepted2, totalScore2);
+
+        assertEquals(0, totalScore1);
+        assertEquals(300, totalScore2);
+        assertEquals(123, totalScore3);
     }
 
 
